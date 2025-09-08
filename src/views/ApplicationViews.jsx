dde13d919d37373000bdbdc33b0f11a79bfea3bb
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-08 14:33:15 -0500

    initial

---
+++ src/views/ApplicationViews.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const ApplicationViews = () => {
+    return <div>ApplicationViews</div>
+}
---
+++ src/views/ArtistView.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const ArtistView = () => {
+    return <div>ArtistView</div>
+}
