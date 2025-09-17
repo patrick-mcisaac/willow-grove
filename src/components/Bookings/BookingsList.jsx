ef058697d5586d1da898751d5fb7a01a9a92eaa6
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-17 13:50:25 -0500

    put the buttons inside the booking card to delete or edit

--- src/components/Bookings/BookingsList.jsx
+++ src/components/Bookings/BookingsList.jsx
@@ -15,7 +15,7 @@ export const BookingsList = ({ booking, setBookings, getBookings }) => {
     }
     return (
         <div className="relative flex flex-col items-center">
-            <div className="shadow-blue border-light-blue flex h-[8rem] w-[30rem] flex-col justify-evenly rounded-2xl border-[.1rem] shadow-md">
+            <div className="shadow-blue border-light-blue flex w-[30rem] flex-col justify-start gap-[2rem] rounded-2xl border-[.1rem] p-[1rem] shadow-md">
                 <div className="flex justify-around">
                     <p className="text-[1.5rem] font-semibold">
                         {booking.date}
@@ -27,25 +27,25 @@ export const BookingsList = ({ booking, setBookings, getBookings }) => {
                 <p className="text-center text-[1.3rem] font-semibold">
                     {booking.eventType.name}
                 </p>
+                {currentUser === parseInt(id) && (
+                    <div className="flex w-full items-center justify-evenly">
+                        <button
+                            onClick={() =>
+                                navigate(
+                                    `/artists/${id}/booking/${booking.id}/edit`
+                                )
+                            }
+                            className="shadow-dark bg-blue-grey hover:bg-light-blue text-dark hover:text-light h-[3rem] w-[10rem] cursor-pointer rounded-2xl font-bold tracking-wider shadow-sm transition hover:scale-105"
+                        >
+                            Edit
+                        </button>
+                        <i
+                            onClick={handleDelete}
+                            className="fa-regular fa-trash-can cursor-pointer text-[2.5rem] transition hover:scale-105 hover:text-red-500"
+                        ></i>
+                    </div>
+                )}
             </div>
-            {currentUser === parseInt(id) && (
-                <div className="mt-[2rem] flex w-full items-center justify-evenly">
-                    <button
-                        onClick={() =>
-                            navigate(
-                                `/artists/${id}/booking/${booking.id}/edit`
-                            )
-                        }
-                        className="shadow-dark bg-blue-grey hover:bg-light-blue text-dark hover:text-light h-[3rem] w-[10rem] cursor-pointer rounded-2xl font-bold tracking-wider shadow-sm transition hover:scale-105"
-                    >
-                        Edit
-                    </button>
-                    <i
-                        onClick={handleDelete}
-                        className="fa-regular fa-trash-can cursor-pointer text-[2.5rem] transition hover:scale-105 hover:text-red-500"
-                    ></i>
-                </div>
-            )}
         </div>
     )
 }
