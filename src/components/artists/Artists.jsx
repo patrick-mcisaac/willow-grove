085e38f9af2ff60579d945ef1af65931da972584
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-22 10:23:49 -0500

    artist css

--- src/components/artists/Artists.jsx
+++ src/components/artists/Artists.jsx
@@ -15,15 +15,15 @@ export const Artists = () => {
         setFilteredArtists(artists)
     }, [artists])
     return (
-        <div className="flex flex-col items-center gap-[5rem] p-[5rem]">
-            <h1 className="text-[5rem] font-bold tracking-wider">
+        <div className="flex flex-col items-center gap-[2.5rem] p-[3rem] md:gap-[4rem] lg:gap-[5rem] lg:p-[5rem]">
+            <h1 className="text-center text-[2.3rem] font-bold tracking-wider md:text-[4rem] lg:text-[5rem]">
                 Our Artists
             </h1>
             <FilterBar
                 setFilteredArtists={setFilteredArtists}
                 artists={artists}
             />
-            <div className="flex flex-wrap items-center justify-around gap-[5rem_4rem]">
+            <div className="flex w-full flex-wrap items-center justify-around gap-[5rem_4rem]">
                 {filteredArtists.map(a => (
                     <ArtistsList key={a.id} artist={a} />
                 ))}
